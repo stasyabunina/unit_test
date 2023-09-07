631a5f5931f58faa81c7329b708cbf22e28dc4ff
Anastasia <tryes9856@gmail.com>
2023-09-07 05:54:34 +0300

    fix

--- src/js/health.js
+++ src/js/health.js
@@ -1,9 +1,8 @@
 export default function showHealth(object) {
   if (object.health > 50) {
     return 'healthy';
-  } else if (object.health <= 50 && object.health > 15) {
+  } if (object.health <= 50 && object.health > 15) {
     return 'wounded';
-  } else {
-    return 'critical';
   }
+  return 'critical';
 }
